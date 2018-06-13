16cf2f1e717f032726350776ee2e2600f127cf81
sca666 <775180138@qq.com>
2018-06-13 15:04:10 +0800

    first commit

---
+++ cy-common/src/main/java/xin/cymall/oss/OSSFactory.java
@@ -0,0 +1,36 @@
+package xin.cymall.oss;
+
+import xin.cymall.utils.ConfigConstant;
+import xin.cymall.utils.Constant;
+import xin.cymall.utils.SpringContextUtils;
+import xin.cymall.service.SysConfigService;
+
+/**
+ * 文件上传Factory
+ * @author chenyi
+ * @email 228112142@qq.com
+ * @date 2017-03-26 10:18
+ */
+public final class OSSFactory {
+    private static SysConfigService sysConfigService;
+
+    static {
+        OSSFactory.sysConfigService = (SysConfigService) SpringContextUtils.getBean("sysConfigService");
+    }
+
+    public static CloudStorageService build(){
+        //获取云存储配置信息
+        CloudStorageConfig config = sysConfigService.getConfigObject(ConfigConstant.CLOUD_STORAGE_CONFIG_KEY, CloudStorageConfig.class);
+
+        if(config.getType() == Constant.CloudService.QINIU.getValue()){
+            return new QiniuCloudStorageService(config);
+        }else if(config.getType() == Constant.CloudService.ALIYUN.getValue()){
+            return new AliyunCloudStorageService(config);
+        }else if(config.getType() == Constant.CloudService.QCLOUD.getValue()){
+            return new QcloudCloudStorageService(config);
+        }
+
+        return null;
+    }
+
+}
